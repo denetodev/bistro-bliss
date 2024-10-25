caacf5f58ef6b18dea8641c428f9b31008090e22
Torres-Neto <torresneto.tn@gmail.com>
2024-10-24 21:36:56 -0300

    trabalhando no header

--- frontend/bistro-bliss/src/app/app-routing.module.ts
+++ frontend/bistro-bliss/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { ReservationComponent } from './features/reservation/reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contato', component: ContactComponent },
+  { path: 'reserva', component: ReservationComponent },
+  { path: '**', redirectTo: '' }, // Redireciona para home em caso de rota não encontrada
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/bistro-bliss/src/app/app.module.ts
+++ frontend/bistro-bliss/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MenubarModule } from 'primeng/menubar';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { ReservationComponent } from './features/reservation/reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    MenuComponent,
+    BlogComponent,
+    ContactComponent,
+    ReservationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenubarModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
