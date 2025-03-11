f355ea15c6d7062d95efce21830fbeb5494118ba
Torres-Neto <torresneto.tn@gmail.com>
2025-03-11 16:47:47 -0300

    ajustes de páginas prontos

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -8,46 +10,41 @@ import { MenuItem } from 'primeng/api';
 })
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
+  menuOpen = false;
+
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller
+  ) {}
 
   ngOnInit() {
     this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-home',
-        routerLink: '/',
-      },
-      {
-        label: 'Sobre',
-        icon: 'pi pi-info-circle',
-        routerLink: '/sobre',
-      },
-      {
-        label: 'Menu',
-        icon: 'pi pi-book',
-        routerLink: '/menu',
-      },
-      {
-        label: 'Blog',
-        icon: 'pi pi-pencil',
-        routerLink: '/blog',
-      },
-      {
-        label: 'Contato',
-        icon: 'pi pi-envelope',
-        routerLink: '/contato',
-      },
+      { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
+      { label: 'Sobre', icon: 'pi pi-info-circle', routerLink: '/sobre' },
+      { label: 'Menu', icon: 'pi pi-book', routerLink: '/menu' },
+      { label: 'Blog', icon: 'pi pi-pencil', routerLink: '/blog' },
+      { label: 'Contato', icon: 'pi pi-envelope', routerLink: '/contato' },
       {
         label: 'Reserva',
         icon: 'pi pi-calendar',
         routerLink: '/reserva',
-        styleClass: 'special-item', // Vamos usar isso para estilizar o botão de reserva
+        styleClass: 'special-item',
       },
     ];
+
+    // Sempre rola para o topo quando a navegação termina
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]); // Leva para o topo
+      }
+    });
   }
 
-  menuOpen = false; // Define a propriedade menuOpen
-
   toggleMenu() {
-    this.menuOpen = !this.menuOpen; // Alterna o estado do menu
+    this.menuOpen = !this.menuOpen;
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,107 +1,53 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from '../../app-routing.module';
-import { MenubarModule } from 'primeng/menubar';
-
-// Componentes do Core
-import { HeaderComponent } from '../../core/header/header.component';
-import { FooterComponent } from '../../core/footer/footer.component';
-
-// Componentes das Features
-import { AppComponent } from '../../app.component';
-import { HomeComponent } from '../../features/home/home.component';
-import { AboutComponent } from '../../features/about/about.component';
-import { MenuComponent } from '../../features/menu/menu.component';
-import { ContactComponent } from '../../features/contact/contact.component';
-import { ReservationComponent } from '../../features/reservation/reservation.component';
-
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Módulos do PrimeNG
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { HeroComponent } from '../../features/home/hero/hero.component';
-import { MenuSectionComponent } from '../../features/home/menu-section/menu-section.component';
-import { ServicesSectionComponent } from '../../features/home/services-section/services-section.component';
-import { TestimonialsComponent } from '../../features/home/testimonials/testimonials.component';
-import { DeliverySectionComponent } from '../../features/home/delivery-section/delivery-section.component';
-import { MenuCardComponent } from '../../shared/components/menu-components/menu-card/menu-card.component';
-import { MenuCardHomeComponent } from '../../shared/components/home-components/menu-card-home/menu-card-home.component';
-import { AboutSectionComponent } from '../../features/home/about-section/about-section.component';
-import { ImageAndCardComponent } from '../../shared/components/home-components/image-and-card/image-and-card.component';
-import { TestimonialCardComponent } from '../../shared/components/home-components/testimonial-card/testimonial-card.component';
-import { AboutSectionPrincipalComponent } from '../../features/about/about-section-principal/about-section-principal.component';
-import { AboutRestaurantAmbienceComponent } from '../../features/about/about-restaurant-ambience/about-restaurant-ambience.component';
 import { DialogModule } from 'primeng/dialog';
-import { ClientInfoComponent } from '../../features/about/client-info/client-info.component';
-import { DeliveryAppsComponent } from '../../features/menu/delivery-apps/delivery-apps.component';
-import { MenuPageComponent } from '../../features/menu/menu-page/menu-page.component';
-import { BlogHomeSectionComponent } from '../../features/home/blog-home-section/blog-home-section.component';
-import { HomeBlogCardComponent } from '../../shared/components/home-components/home-blog-card/home-blog-card.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+
+// Módulos do Projeto
 import { SharedModule } from '../../shared/components/shared.module';
 import { BlogModule } from '../../features/blog/blog.module';
-import { HttpClientModule } from '@angular/common/http';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { AuthModule } from '../../features/auth/auth.module';
+
+// Componentes Exclusivos do Admin (se houver)
+import { DashboardComponent } from './pages/dashboard/dashboard.component'; // Exemplo
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    AboutComponent,
-    MenuComponent,
-    ContactComponent,
-    ReservationComponent,
-    HeroComponent,
-    MenuSectionComponent,
-    ServicesSectionComponent,
-    TestimonialsComponent,
-    DeliverySectionComponent,
-    MenuCardComponent,
-    AboutSectionComponent,
-    ImageAndCardComponent,
-    TestimonialCardComponent,
-    AboutSectionPrincipalComponent,
-    AboutRestaurantAmbienceComponent,
-    ClientInfoComponent,
-    DeliveryAppsComponent,
-    MenuPageComponent,
-    MenuCardHomeComponent,
-    BlogHomeSectionComponent,
-    HomeBlogCardComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MenubarModule,
-    RouterModule,
     CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+
+    // Módulos do PrimeNG
     ButtonModule,
     CardModule,
     DialogModule,
-    HttpClientModule,
-    AuthModule,
-
-    ReactiveFormsModule,
     CalendarModule,
     DropdownModule,
     InputNumberModule,
     InputTextareaModule,
+
+    // Módulos do Projeto
     SharedModule,
     BlogModule,
-    FormsModule,
+    AuthModule,
   ],
-  exports: [],
   providers: [provideHttpClient(withFetch())],
-  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AdminModule {}
